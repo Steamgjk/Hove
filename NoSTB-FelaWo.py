4303a94e3e84dde5aa03bb7aafd16e6ad26a96d4
steamgjk <steam1994@163.com>
2020-01-06 11:48:57 -0800

    debug

--- NoSTB-FelaWo.py
+++ NoSTB-FelaWo.py
@@ -355,13 +355,13 @@ def check_dependency(my_depth, my_token_no):
     pre_depth = my_depth -1
     for i in range(sta, sta+TOKEN_WEIGHT[my_depth]):
         if CHUNK_HOLD_MAP[pre_depth][i] == 0:
-            #print("fp no :", int(my_depth),"\t",int(my_token_no),"\t", int(pre_depth),"\t", int(i) )
+            print("fp no :", int(my_depth),"\t",int(my_token_no),"\t", int(pre_depth),"\t", int(i) )
             return False
     if is_bp_depth(my_depth):
         fp_depth = TOKEN_LAYERS-1 - my_depth
         for i in range(sta, sta+TOKEN_WEIGHT[my_depth]):
             if CHUNK_HOLD_MAP[fp_depth][i] == 0:
-                #print("bp no :", int(my_depth),"\t",int(my_token_no),"\t", int(fp_depth),"\t", int(i) )
+                print("bp no :", int(my_depth),"\t",int(my_token_no),"\t", int(fp_depth),"\t", int(i) )
                 return False
     return True       
 
